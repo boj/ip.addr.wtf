f84055154c031f29020883c885f8dc01a83b3d43
Brian Jones <mojobojo@gmail.com>
2014-08-13 15:26:58 +0900

    a dash of logging

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"io"
 	"log"
+	"log/syslog"
 	"net/http"
+	"os"
 	"strings"
 )
 
+var Logger *log.Logger
+
+func init() {
+	sl, err := syslog.Dial("", "", syslog.LOG_ERR, "")
+	if err != nil {
+		log.Print(err.Error())
+		os.Exit(1)
+	}
+	Logger = log.New(sl, "[ip.addr.wtf] ", 0)
+}
+
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	addr := strings.Split(r.RemoteAddr, ":")[0]
+	Logger.Print(addr)
 	io.WriteString(w, addr)
 }
 
